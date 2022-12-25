7f1f40caede36859e00186aa4d8d5fa2a7c7bfeb
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2022-12-25 18:59:20 +0100

    add routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import Home from 'pages/Home/Home';
+import Events from 'pages/Events/Events';
+import EventSubPage from 'pages/EventSubPage/EventSubPage';
+import Search from 'pages/Search/Search';
+import EventDetailsPage from 'pages/EventDetailsPage/EventDetailsPage';
+import Test from './Test/Test';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="events" element={<Events />}>
+          <Route path=":id" element={<EventSubPage />} />
+        </Route>
+        <Route path="events/:id/details" element={<EventDetailsPage />} />
+        <Route path="search" element={<Search />}>
+          <Route path=":id" element={<EventSubPage />} />
+        </Route>
+        <Route path="search/:id/details" element={<EventDetailsPage />}>
+          <Route path="test" element={<Test />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
